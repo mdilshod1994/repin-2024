78e65fb1d95e048e9d202350c3952b35cd312e3a
mdilshod1994 <mdilshod1994@gmail.com>
2024-03-08 00:52:25 +0500

    добавил gap для компонента r-grid чтобы можно было переиспользовать

--- components/home/portfolio.vue
+++ components/home/portfolio.vue
@@ -18,7 +18,14 @@
         <div class="tab">Crypto</div>
       </r-carousel>
     </div>
-    <r-grid button :tablet-column="2" cls="portfolio__grid">
+    <r-grid
+      button
+      :tablet-column="2"
+      :desktop-gaps="[60, 24]"
+      :tablet-gaps="[48, 20]"
+      :mobile-gaps="[32, 8]"
+      cls="portfolio__grid"
+    >
       <portfolio-card v-for="c in 6" cls="portfolio__card" />
     </r-grid>
   </div>
--- components/r/grid.vue
+++ components/r/grid.vue
@@ -1,17 +1,41 @@
 <script setup lang="ts">
-withDefaults(
+const props = withDefaults(
   defineProps<{
     button?: boolean
     mobileColumn?: number
     tabletColumn?: number
     desktopColumn?: number
+    desktopGaps?: number[] // [row, column]
+    tabletGaps?: number[] // [row, column]
+    mobileGaps?: number[] // [row, column]
   }>(),
   {
     mobileColumn: 2,
     desktopColumn: 3,
     tabletColumn: 3,
+    desktopGaps: () => [24, 24],
+    tabletGaps: () => [20, 20],
+    mobileGaps: () => [8, 8],
   },
 )
+
+const gaps = (gaps: number[]) => {
+  if (gaps.length > 1) {
+    return `${gaps[0]}px ${gaps[1]}px`
+  } else {
+    return `${gaps[0]}px`
+  }
+}
+
+const desktopGap = computed(() => {
+  return gaps(props.desktopGaps)
+})
+const tabletGap = computed(() => {
+  return gaps(props.tabletGaps)
+})
+const mobileGap = computed(() => {
+  return gaps(props.mobileGaps)
+})
 </script>
 
 <template>
@@ -32,8 +56,7 @@ withDefaults(
   &__wrap {
     display: grid;
     grid-template-columns: repeat(v-bind(desktopColumn), 1fr);
-    grid-row-gap: 60px;
-    grid-column-gap: 24px;
+    gap: v-bind(desktopGap);
     width: 100%;
   }
 }
@@ -43,8 +66,7 @@ withDefaults(
     gap: 64px;
     &__wrap {
       grid-template-columns: repeat(v-bind(tabletColumn), 1fr);
-      grid-row-gap: 48px;
-      grid-column-gap: 20px;
+      gap: v-bind(tabletGap);
     }
   }
 }
@@ -53,8 +75,7 @@ withDefaults(
     gap: 56px;
     &__wrap {
       grid-template-columns: repeat(v-bind(mobileColumn), 1fr);
-      grid-row-gap: 40px;
-      grid-column-gap: 8px;
+      gap: v-bind(mobileGap);
     }
   }
 }
