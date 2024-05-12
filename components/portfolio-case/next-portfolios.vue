82c92f322c1c49fc7eec6d0cb8b84bce3be004d6
mdilshod1994 <mdilshod1994@gmail.com>
2024-05-13 03:39:56 +0500

    анимация следующих кейсов (там где лесенка)

--- components/portfolio-case/next-portfolios.vue
+++ components/portfolio-case/next-portfolios.vue
@@ -1,38 +1,40 @@
 <script setup lang="ts">
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
 import type { PortfolioElement } from "~/types/portfolio"
 
 const store = usePortfolio()
-const marginTop = ref<number>(104)
 const portfolio = ref<HTMLElement | null>(null)
 
 const portfolios = computed(() => {
   return store.portfolio?.slice(0, 3) as PortfolioElement[]
 })
-
-const setMarginFirstEl = computed(() => {
-  return `${marginTop.value}px`
-})
-const setMarginScndEl = computed(() => {
-  return `${marginTop.value * 2}px`
-})
 onMounted(async () => {
   await store.getPortfolio("all", 0)
-
-  function setMargin() {
-    const htmlElement = document.documentElement
-    if (!portfolio.value) return
-    const topPosition = portfolio.value?.offsetTop
-    const scrolledFromTop = htmlElement.scrollTop
-    const topStart = portfolio.value.offsetHeight - window.screen.height
-    const topEnd = htmlElement.scrollTop - portfolio.value.offsetTop
-    const per = (topEnd / topStart) * 100
-    if (topPosition <= scrolledFromTop && per <= 100) {
-      marginTop.value = 104 - (104 * Math.ceil(per)) / 100
-    }
-  }
-  window.addEventListener("resize", setMargin)
-  window.addEventListener("scroll", setMargin)
-  window.addEventListener("DOMContentLoaded", setMargin)
+  gsap.registerPlugin(ScrollTrigger)
+  gsap.to(".first", {
+    scrollTrigger: {
+      trigger: ".first",
+      start: "-30% 60%",
+      end: "10% 40%",
+      scrub: 2,
+    },
+    y: -104,
+    ease: "power1.inOut",
+    duration: 3,
+  })
+  gsap.to(".second", {
+    scrollTrigger: {
+      trigger: ".first",
+      start: "-30% 60%",
+      end: "10% 40%",
+      scrub: 2,
+    },
+    y: -208,
+    ease: "power1.inOut",
+    duration: 3,
+  })
 })
 </script>
 
@@ -42,10 +44,11 @@ onMounted(async () => {
       <r-title pretitle="Our projects" title="Next projects" flex-start />
       <r-grid mobile-column="1" tablet-column="3" :mobile-gaps="[40]" button>
         <portfolio-card
-          v-for="portfolio in portfolios"
+          v-for="(portfolio, idx) in portfolios"
           :key="portfolio.id"
           :portfolio="portfolio"
           cls="project"
+          :class="`${idx === 1 ? 'first' : ''} ${idx === 2 ? 'second' : ''} `"
         />
       </r-grid>
     </div>
@@ -55,10 +58,7 @@ onMounted(async () => {
 <style module lang="scss">
 .projects {
   padding: 72px 0 88px;
-  height: 180svh;
   &__inner {
-    position: sticky;
-    top: 0px;
     display: flex;
     flex-direction: column;
     gap: 64px;
@@ -66,21 +66,10 @@ onMounted(async () => {
 }
 .project {
   &:nth-child(2) {
-    margin-top: v-bind(setMarginFirstEl);
+    margin-top: 104px;
   }
-  &:last-child {
-    margin-top: v-bind(setMarginScndEl);
-  }
-}
-@include mobile {
-  .projects {
-    height: auto;
-  }
-  .project {
-    &:last-child,
-    &:nth-child(2) {
-      margin-top: 0;
-    }
+  &:nth-child(3) {
+    margin-top: 208px;
   }
 }
 </style>
